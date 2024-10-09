fb5d866cc58c1337df0a12ca602bb265344ee956
Richard Allen <Richard.Allen@ubisoft.com>
2024-10-09 15:49:07 +0100

    Setup blank project

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
+#include <catch2/catch_session.hpp>
 
-int main()
+#include "statepatternexamples.h"
+
+int main(const int argc, const char* const argv[])
 {
+    return Catch::Session().run(argc, argv);
 }
